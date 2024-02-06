36852f2cd9d3e560c6541405387b835712775989
vikasrajnutriful <vikas@ntrtrust.org>
2024-02-06 20:51:31 +0530

    Task: Added RazorPay

--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,11 @@ import simplejson as json
 from .utils import generate_order_number
 from account.utils import send_notifaction
 from django.contrib.auth.decorators import login_required
+import razorpay
+from foodonline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
 
+
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
 
 @login_required(login_url = "login")
 def place_order(request):
@@ -41,9 +45,24 @@ def place_order(request):
             order.save() # this will generate id/pk
             order.order_number = generate_order_number(order.id)
             order.save()
+            DATA = {
+                    "amount": float(order.total) * 100,
+                    "currency": "INR",
+                    "receipt": "receipt#"+order.order_number,
+                    "notes": {
+                        "key1": "value3",
+                        "key2": "value2"
+                    }
+                }
+            rzp_order = client.order.create(data=DATA)
+            rzp_order_id = rzp_order["id"]
+            print(rzp_order)
             context = {
                 'order': order,
                 'cart_items':cart_items,
+                "rzp_order_id":rzp_order_id,
+                "RZP_KEY_ID": RZP_KEY_ID,
+                "rzp_amount": float(order.total) * 100,
             }
             return render(request, "orders/place_order.html",context)
 
